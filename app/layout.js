0418d851ae0e193c65a5fda80c52d0ca18f4f875
wagolemusa <homiemusa@gmail.com>
2024-03-30 02:24:39 +0300

    Worked on Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/layouts/Header'
-import Header2 from '../components/layouts/Header2'
+import Header3 from '../components/layouts/Header3'
 import Footer from '../components/layouts/Footer'
 import { GlobalProvider } from './GlobalProvider'
 import HeaderLine from "../components/layouts/HeaderLine"
@@ -18,8 +18,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <GlobalProvider>
-        <HeaderLine />
-        <Header />
+        {/* <HeaderLine /> */}
+        <Header3 />
+        {/* <Header /> */}
         {/* <Header2 /> */}
         {children}
         <Footer />
