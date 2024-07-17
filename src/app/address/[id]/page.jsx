86cdcff52f4fa11859dff0b810f2441f2d4ec0c0
Your Name <you@example.com>
2024-07-17 23:14:02 +0300

    Debug 4

--- src/app/address/[id]/page.jsx
+++ src/app/address/[id]/page.jsx
@@ -3,6 +3,9 @@ import React from "react"
 
 import { cookies } from "next/headers";
 import UpdateAddress from "../../../components/user/UpdateAddress";
+
+const nextCookies = cookies();
+
 const nextAuthSessionToken = nextCookies.get("next-auth.session-token");
 
 const getAddress = async (id) => {
