6cfa80f91d124fa8b7af09fad69535548ab982c3
Your Name <you@example.com>
2024-07-19 23:08:04 +0300

    Debug 4

--- src/app/gas/page.jsx
+++ src/app/gas/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios';
-import Cardslide from "../../components/layouts/Cardslide";
+import Cardslide from "../../components/gas/Cardslide";
 
 
 const getGass = async () => {    
