214888a88105e98f4dd86fc56b6f4263c8d66432
Your Name <you@example.com>
2024-07-20 14:22:21 +0300

    Debug profile

--- src/components/user/UserAddresses.jsx
+++ src/components/user/UserAddresses.jsx
@@ -1,7 +1,7 @@
 
 'use client'
 import Link from "next/link";
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 
 const UserAddresses = () => {
