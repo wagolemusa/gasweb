f9a9d280517740175766694ee44cee6435460a5b
Your Name <you@example.com>
2024-07-20 16:47:50 +0300

    Login Debug

--- src/components/gas/Cardslide.jsx
+++ src/components/gas/Cardslide.jsx
@@ -11,7 +11,7 @@ const Cardslide = () => {
   useEffect(() => {
       async function fetchData() {
           try {
-              const response = await axios.get('https://giggas-30bcd3403c62.herokuappdds.com/api/gas');
+              const response = await axios.get('https://giggas-30bcd3403c62.herokuapp.com/api/gas');
               setData(response.data);
           } catch (error) {
               setError('Failed to fetch Data');
