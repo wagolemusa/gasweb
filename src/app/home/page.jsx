830f7340d3467df696bcfabef03b0783adadf91f
Your Name <you@example.com>
2024-07-23 23:13:00 +0300

    slide

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,12 +2,19 @@
 import React from "react";
 
 import Cardslide from "../../components/gas/Cardslide";
-
+import SwiperComponent from "../../components/layouts/swiper"
+// import HomeSlide from "../../components/layouts/homeSlide"
 const Home = () => {
 
     return (
-        <div className="py-5">
+        <div className="py-10">
+           <br/>  <br/>  <br/>  <br/>  <br/>
+          {/* <HomeSlide /> */}
+         
           <Cardslide />
+          <br/>  <br/>  <br/>
+          <SwiperComponent />
+          
         </div>
     )
 }
--- src/components/gas/Cardslide.jsx
+++ src/components/gas/Cardslide.jsx
@@ -1,17 +1,29 @@
 'use client'
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState, Suspense, useRef } from "react";
 import axios from "axios";
 import Image from "next/image";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Cardslide = () => {
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
+  // const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    // progressCircle.current.style.setProperty('--progress', 1 - progress);
+    // progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
 
   useEffect(() => {
       async function fetchData() {
           try {
-              const response = await axios.get('https://giggas-30bcd3403c62.herokuapp.com/api/gas');
+              const response = await axios.get('http://localhost:3000/api/gas');
               setData(response.data);
           } catch (error) {
               setError('Failed to fetch Data');
@@ -20,46 +32,93 @@ const Cardslide = () => {
       }
       fetchData()
   }, [])
-
+  // SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
  
-
   return(
 
     <Suspense>
-      <div className='container'>
-        <div className='row'>
-       
-          { data?.get6kgs?.map((gas6kg) =>(
+      <div className="container-fluid">
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={5}
+            // navigation
+            // pagination={{ clickable: true }}
+            // scrollbar={{ draggable: true }}
+            // autoplay={{ delay: 2500, disableOnInteraction: false }}
 
-            <div className='col-md-4'>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-              <Image
-                src={
-                  gas6kg?.images[0]
-                    ? gas6kg?.images[0].url
-                    : "/images/default_product.png"
-                }
-                alt="product anme"
-                height="240"
-                width="240"
-              />
-                <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">{gas6kg?.seton}</div>
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            onAutoplayTimeLeft={onAutoplayTimeLeft}
+            className="mySwiper"
+
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              480: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 25,
+              },
+              1280: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+              }
+            }}
+          >
+            { data?.get6kgs?.map((gas6kg) =>(
+            <SwiperSlide>
+              <div className="slide6 max-w-sm rounded overflow-hidden shadow-lg w-48">
+                <Image
+                  src={
+                    gas6kg?.images[0]
+                      ? gas6kg?.images[0].url
+                      : "/images/default_product.png"
+                  }
+                  alt="product anme"
+                  height="240"
+                  width="240"
+                />
+                  
+                  <div className="font-bold text-xl py-3">{gas6kg?.productName} &nbsp;  {gas6kg?.cylinderSize}</div>
+                  
+                  <div class="pt-2 pb-10">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2  line-through">UGX {gas6kg?.price}</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2  ">UGX {gas6kg?.discount}</span>
+                  </div>
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-              </div>
-            </div>
-
-          ))}
-
-          </div>
-
-      </div>
+                <button className="btnStep1">Add To Cart</button>
+                </SwiperSlide>
+              ))}
+               {/* <div className="autoplay-progress" slot="container-end"> */}
+                {/* <svg viewBox="0 0 48 48" ref={progressCircle}>
+                  <circle cx="24" cy="24" r="20"></circle>
+                </svg> */}
+                {/* <span ref={progressContent}></span> */}
+              {/* </div> */}
+        </Swiper>
+        </div>
     </Suspense>
+
   )
 }
 
